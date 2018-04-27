c4eacfeb823925bc97b6e7721d292ffa69374766
Toby Hughes <tobywhughes@gmail.com>
2018-04-26 18:52:39 -0700

    Caches pcap files already seen.

--- pcap_parser.py
+++ pcap_parser.py
@@ -5,9 +5,14 @@ Converts PCAP files and converts them into a time series format.
 Tested and optimized to be used with the CAIDA Anonymized data set.
 """
 import dpkt
+import matplotlib.pyplot as plt
+import os
+import pickle
 
 def import_pcap_list(filename, print_num = 3000000, print_progress = True):
     print("Parsing file: ", filename)
+    if exists_in_cache(filename):
+        return read_pcap_cache(filename)
     f = open(filename, mode = 'rb')
     pcap = dpkt.pcap.Reader(f)
     packets = 0
@@ -19,22 +24,48 @@ def import_pcap_list(filename, print_num = 3000000, print_progress = True):
         if packets % print_num == 0 and print_progress:
             print("Packets Processed: ", packets)
     print("Total Packets Processed: ", packets)
+    cache_pcap_import(packet_list, filename)
     f.close()
     return packet_list
+
+def cache_pcap_import(packet_list, filename):
+    stripped_name = os.path.basename(filename).rstrip(".anon.pcap")
+    pickle_name = '.\\cache\\' +stripped_name + '.p'
+    os.makedirs(os.path.dirname(pickle_name), exist_ok=True)
+    pickle.dump(packet_list, open(pickle_name, 'wb'))
+    print("Cached in ", os.path.abspath(pickle_name))
+
+def exists_in_cache(filename):
+    stripped_name = os.path.basename(filename).rstrip(".anon.pcap")
+    pickle_name = '.\\cache\\' +stripped_name + '.p'
+    os.makedirs(os.path.dirname(pickle_name), exist_ok=True)
+    return os.path.isfile(pickle_name)
+
+def read_pcap_cache(filename):
+    stripped_name = os.path.basename(filename).rstrip(".anon.pcap")
+    pickle_name = '.\\cache\\' +stripped_name + '.p'
+    print("Read in cache from", os.path.abspath(pickle_name))
+    return pickle.load(open(pickle_name, 'rb'))
+
 
 def convert_to_seconds_series(packet_list):
     current_packet_count = 0
     last_packet_time = 0
     packet_times = []
     start_time = packet_list[0]
+    update_flag = False
     for packet in packet_list:
         current_packet_time = packet - start_time
         if current_packet_time == last_packet_time:
             current_packet_count += 1
+            update_flag = False
         else:
             packet_times.append((packet - 1, current_packet_count))
             current_packet_count = 1
+            update_flag == True
         last_packet_time = current_packet_time
+    if update_flag == False:
+        packet_times.append((packet_list[-1], current_packet_count))
     return packet_times
 
 def move_to_zero(packet_list):
@@ -63,15 +94,36 @@ def read_list_of_names(filename):
         if line[0] != '#':
             filename_list.append(line.rstrip())
     return filename_list
+    f.close()
 
 def output_series_by_line(packet_list):
     for times in packet_times:
         print("Time: ", times[0], "  Count: ", times[1])
 
+def run_pcap_imports(filename_list):
+    packet_lists = []
+    for filename in filename_list:
+        packet_lists.append(import_pcap_list(filename))
+    return packet_lists
+
+def flatten_list(packet_lists):
+    packet_list = []
+    for current_list in packet_lists:
+        packet_list += current_list
+    return  sorted(packet_list)
+
+def show_time_series(packet_list):
+    times = [second[0] for second in packet_list]
+    counts =[count[1] for count in packet_list]
+    plt.plot(times, counts)
+    plt.axis([0, len(times), 0, (max(counts) * 1.1)])
+    plt.show()
+
 if __name__ == "__main__":
     name_list = read_list_of_names("./pcap_list.txt")
-    packet_list = import_pcap_list(name_list[0])
-    packet_list2 = import_pcap_list(name_list[1])
+    packet_lists = run_pcap_imports(name_list)
+    packet_list = flatten_list(packet_lists)
     packet_times = move_to_zero(convert_to_seconds_series(packet_list))
-    padded_packet_times = zero_pad(packet_times, 60)
-    output_series_by_line(padded_packet_times)
+    padded_packet_times = zero_pad(packet_times, 180)
+    #output_series_by_line(padded_packet_times)
+    show_time_series(padded_packet_times)
